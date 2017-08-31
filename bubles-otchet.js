9bf9dd706c5325a3fe06f7583ea130feacc8fa6f
aliano <jilook@yandex.ru>
2017-08-31 15:53:29 +0300

    дописана программа про пузыри

--- bubles-otchet.js
+++ bubles-otchet.js
@@ -1,14 +1,22 @@
 var searchBubble = [];
 var numResearch = [];
+var quantyStuff = [];
+var numEffection = [];
 var maxBubble = 0;
+var maxEffection = 0;
 
 
 function searchQB (numberSearch) {
 	for (var i = 0; i < numberSearch; i++) {
-	 	var k = Math.floor(Math.random()*2);
+	 	var k = Math.floor(Math.random()*60);
+	 	var j = (Math.floor(Math.random()*20)) + 1;
 	 	searchBubble.push(k);
+	 	quantyStuff.push(j);
 	 	if (k > maxBubble) {
 	 		maxBubble = k;
+	 	}
+	 	if ( k / j > maxEffection) {
+	 		maxEffection = k / j;
 	 	}
 
 	 }
@@ -16,11 +24,18 @@ function searchQB (numberSearch) {
 			if (searchBubble[i] == maxBubble) {
 				numResearch.push(i);
 			}
+			if (searchBubble[i]/quantyStuff[i] == maxEffection) {
+				numEffection.push(i);
+
+			}
+
 		}
 
 
+
 }
-searchQB(20);
+searchQB(5);
 console.log(searchBubble);
-console.log(maxBubble);
-console.log(numResearch);
+console.log(quantyStuff);
+console.log("Наибольшее количество пузырей = " + maxBubble + " в растворе = " + numResearch);
+console.log("Максимальная эффективность = " + maxEffection + " в растворе = " + numEffection);
