de1b4131c83ce814832c8216a7e75d4402dc0d50
aliano <jilook@yandex.ru>
2017-09-10 20:56:33 +0300

    glava 6 DOM

---
+++ code.js
@@ -0,0 +1,3 @@
+var domZ = document.getElementById('domZ');
+domZ.innerHTML = 'Red hat my friend';
+alert(domZ.innerHTML);
---
+++ js-lesson-test.js
@@ -0,0 +1,11 @@
+const satellite = 'The Moon';
+const galaxy = 'The Milky Way';
+var stars = 'North Star';
+
+const myNightSky = () => {
+  stars = 'Sirius';
+  return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
+};
+
+console.log(myNightSky());
+console.log(stars)
